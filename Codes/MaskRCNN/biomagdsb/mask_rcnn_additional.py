f42b8b3fe21d029363cfbf7843d463790780282d
tpecot <36488353+tpecot@users.noreply.github.com>
2021-02-15 16:20:27 -0500

    Add files via upload

--- Codes/MaskRCNN/biomagdsb/mask_rcnn_additional.py
+++ Codes/MaskRCNN/biomagdsb/mask_rcnn_additional.py
@@ -13,7 +13,7 @@ class NucleiConfig(mrcnn_config.Config):
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
     NUM_CLASSES = 1 + 1 # background + nucleus
-    TRAIN_ROIS_PER_IMAGE = 512
+    TRAIN_ROIS_PER_IMAGE = 128
     STEPS_PER_EPOCH = 5000 # check mask_train for the final value
     VALIDATION_STEPS = 50
     DETECTION_MAX_INSTANCES = 512
